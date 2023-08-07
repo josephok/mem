8bd4bab04d9eb809e60a20c3ea057f8e3721ec9f
Joseph <josephok@qq.com>
2023-08-07 13:57:36 +0800

    change default READ_COUNT to 100

--- read_u64.c
+++ read_u64.c
@@ -8,12 +8,3 @@ uint64_t read_u64(int fd, off_t offset)
     return data;
 }
 
-ssize_t read_u64_n(int fd, off_t offset, uint64_t data[], size_t n)
-{
-
-    ssize_t nread;
-    if ((nread = pread(fd, data, sizeof(uint64_t) * n, offset)) < 0)
-        return -1;
-
-    return nread / sizeof(uint64_t);
-}
